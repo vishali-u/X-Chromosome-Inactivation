4bc1d5fa280b6c75e568eb40c74252d5e3401bb2
Vishali Umaiyalan <vishali.umaiyalan@mail.utoronto.ca>
2023-07-05 11:36:47 -0400

    added to find_hSNPs.R

--- hca_kidcortex/find_hSNPs.R
+++ hca_kidcortex/find_hSNPs.R
@@ -5,27 +5,26 @@ library(Rsamtools)
 library(seqinr)
 library(devtools)
 
-# Load reference genome
+setwd('/home/umaiyal1/')
+
+# Load reference genome (change to genome.fa when done testing)
 refGenomePath <- '/home/umaiyal1/scratch/ref_package/GRCh38/fasta/genome.fa'
 
-bamsPath <- '/home/umaiyal1/scratch/hca_kidcortex/bams'
+bam1 <- '/home/umaiyal1/scratch/hca_blood/bams/JP_RIK_B001_L001_5GEX_H005.R1Aligned.sortedByCoord.out.bam'
+bam2 <- '/home/umaiyal1/scratch/hca_blood/bams/JP_RIK_B001_L001_5GEX_H005.R2Aligned.sortedByCoord.out.bam'
 
 nParallel <- 8
 
+names <- c('JP_RIK_B001_L001_5GEX_H005.R1', 'JP_RIK_B001_L001_5GEX_H005_R2')
+
+bams10X <- c(bam1, bam2)
+
+bams10X <- setNames(bams10X, names)
+
+bams10X
+
 # Load in BAM files for Control 5 (correspond to a female donor)
-
-bam1 <- scanBam(paste(bamsPath, '/Cont5_S8_L004_R1_001Aligned.sortedByCoord.out.bam', sep = ''))
-names(bam1[[1]])
-
-bam2 <- scanBam(paste(bamsPath, '/Cont5_S8_L004_R2_001Aligned.sortedByCoord.out.bam', sep = ''))
-names(bam1[[1]])
-
-bams <- c(bam1, bam2)
-names(bams)
-
-refGenome <- read.fasta(refGenomePath)
-
-hSNPs <- hetSNPsFromRNA(bams,
-                        refGenome,
-                        outputs = sprintf('%s_scRNA_1kSNPs_XCnts.tsv', names(bams)),
+hSNPs <- hetSNPsFromRNA(bams10X,
+                        refGenomePath,
+                        outputs = sprintf('%s_scRNA_1kSNPs_XCnts.tsv', names(bams10X)),
                         nParallel = nParallel)
